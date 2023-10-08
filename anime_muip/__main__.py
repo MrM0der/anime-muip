eca8ff21219636d0f8fa7f2eaefe18224ad5bf3a
Orzxiety <83078982+Orzxiety@users.noreply.github.com>
2023-10-09 00:27:26 +0500

    modified:   anime_muip/__main__.py

--- anime_muip/__main__.py
+++ anime_muip/__main__.py
@@ -3,6 +3,27 @@ import hashlib
 
 class AnimeMUIP():
     def __init__(self, secret: str, ssl: bool = False, ip: str = '127.0.0.1', port: int = 21051, ticket: str = 'GM', region: str = 'dev_docker', cmd_id: int = 1116):
+        """
+        Creates a client to process MUIP server commands
+
+        Parameters
+        -----------
+        secret: str
+            Authentication token on MUIP server.
+        ssl: bool
+            Whether to use SSL.
+        ip: str
+            Server IP.
+        port: int
+            Server port.
+        ticket: str
+            Server ticket.
+        region: str
+            Server region.
+        cmd_id: int
+            Server cmd_id.
+        """
+
         self.secret = secret
 
         self.url = f'{"https" if ssl == True else "http"}://{ip}:{str(port)}/api'
@@ -15,12 +36,23 @@ class AnimeMUIP():
         sha256.update(f"{message}{secret}".encode())
         return sha256.hexdigest()
 
-    def muip_client(self, uid: str, command: str):
+    def muip_client(self, uid: int, command: str):
+        """
+        Sends a request to the MUIP server
+
+        Parameters
+        -----------
+        uid: str
+            UID of the user in game.
+        command: str
+            Your command.
+        """
+
         payload = {
             "region": self.region,
             "ticket": self.ticket,
             "cmd": self.cmd_id,
-            "uid": uid,
+            "uid": str(uid),
             "msg": command
         }
 
